003c58e1520908d63fde2541fd6d810f2272b11f
Pascual <pascualgorrita@gmail.com>
2021-11-15 19:31:33 +0000

    Test

--- school/models/models.py
+++ school/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import collections
 from odoo import models, fields, api
 
 
@@ -9,29 +10,26 @@ class student(models.Model):
 
 
     name = fields.Char()
-    classrooms = fields.Many2one('school.classroom', ondelete='set null')
+    classroom = fields.Many2one('school.classroom', ondelete='set null', help='La clase a la que va el alumno')
 
 class classroom(models.Model):
     _name = 'school.classroom'
     _description = 'school.classroom'
 
     name = fields.Char()
-    students = fields.One2many('school.student', 'classrooms')
-    teachers = fields.Many2many('school.teacher')
+    students = fields.One2many(comodel_name='school.student', inverse_name='classroom')
+    teachers = fields.Many2many(comodel_name='school.teacher',
+                                relation='teachers_classrooms',
+                                column1='classroom_id',
+                                column2='school_teacher_id')
 
 class teacher (models.Model):
     _name = 'school.teacher'
     _description = 'school.teacher'
 
     name = fields.Char()
-    classrooms = fields.Many2many('school.classroom')
+    classrooms = fields.Many2many(comodel_name='school.classroom',
+                                relation='teachers_classrooms',
+                                column2='classroom_id',
+                                column1='school_teacher_id')
 
-
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
