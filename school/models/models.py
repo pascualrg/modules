859ec00c06916d89ab203766e940b0be37d8a411
Pascual <pascualgorrita@gmail.com>
2021-11-15 21:41:05 +0000

    test

--- school/models/models.py
+++ school/models/models.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+from odoo import _
+from odoo.exceptions import Warning
 import secrets
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class student(models.Model):
     _name = 'school.student'
@@ -18,8 +23,9 @@ class student(models.Model):
     def _get_password(self):
         print(self)
         for student in self:
-            print(student)
+            #print('\033[93m]',student,'\033[93m]')
             student.password = secrets.token_urlsafe(12)
+            _logger.debug('\033[93m]'+str(student)+'\033[93m]')
 
 class classroom(models.Model):
     _name = 'school.classroom'
@@ -31,6 +37,17 @@ class classroom(models.Model):
                                 relation='teachers_classrooms',
                                 column1='classroom_id',
                                 column2='school_teacher_id')
+
+    delegate=fields.Many2one('school.student',compute='_get_delegate')
+    all_teachers=fields.Many2many('school.teacher', compute='_get_teachers')
+
+    def _get_delegate(self):
+        for c in self:
+            c.delegate = c.students[0].id
+    def _get_teachers(self):
+        for c in self:
+            c.all_teachers = c.teachers
+    
 
 class teacher (models.Model):
     _name = 'school.teacher'
