0a25ad2fb1472e0228027f6134ce989b9eefc84f
Pascual <pascualgorrita@gmail.com>
2021-11-15 18:50:36 +0000

    Test

--- school/models/models.py
+++ school/models/models.py
@@ -6,11 +6,10 @@ from odoo import models, fields, api
 class student(models.Model):
     _name = 'school.student'
     _description = 'school.student'
-    
+
 
     name = fields.Char()
     classrooms = fields.Many2one('school.classroom')
-    
 
 class classroom(models.Model):
     _name = 'school.classroom'
