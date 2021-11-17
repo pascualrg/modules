9f1653601b85965eb1086a624b440842a252d494
Pascual <pascualgorrita@gmail.com>
2021-11-17 17:28:44 +0000

    Continuando los tutos

--- school/models/models.py
+++ school/models/models.py
@@ -26,10 +26,13 @@ class student(models.Model):
     last_login = fields.Datetime()
     password = fields.Char(default=lambda s:secrets.token_urlsafe(12))
     dni = fields.Char(string='DNI')
-    profile_pic = fields.Image()
-
-    classroom = fields.Many2one('school.classroom', ondelete='set null', help='La clase a la que va el alumno')
+    profile_pic = fields.Image(max_width="50", max_height="50")
+    is_student = fields.Boolean()
+    classroom = fields.Many2one('school.classroom',  ondelete='set null', help='La clase a la que va el alumno')
     teachers = fields.Many2many('school.teacher', related='classroom.teachers', readonly=True)
+    level = fields.Selection([('1','1'),('2','2')])
+
+    state = fields.Selection([('1','Enrolled'),('2', 'Student'), ('3', 'Ex-Student')],default='1')
 
     @api.constrains('dni')
     def _check_dni(self):
@@ -42,24 +45,25 @@ class student(models.Model):
 
     _sql_constraints = [ ('dni_uniq','unique(dni)','El DNI no se puede repetir') ]
 
+    def regenerate_pass(self):
+        for s in self:
+            password = secrets.token_urlsafe(12)
+            s.write({'password':password})
+
     
 class classroom(models.Model):
     _name = 'school.classroom'
     _description = 'school.classroom'
 
     name = fields.Char()
+    level = fields.Selection([('1','1'),('2','2')])
     students = fields.One2many(comodel_name='school.student', inverse_name='classroom')
     teachers = fields.Many2many(comodel_name='school.teacher',
                                 relation='teachers_classrooms',
                                 column1='classroom_id',
                                 column2='school_teacher_id')
 
-    delegate=fields.Many2one('school.student',compute='_get_delegate')
     all_teachers=fields.Many2many('school.teacher', compute='_get_teachers')
-
-    def _get_delegate(self):
-        for c in self:
-            c.delegate = c.students[0].id
 
     def _get_teachers(self):
         for c in self:
@@ -71,6 +75,8 @@ class teacher (models.Model):
     _description = 'school.teacher'
 
     name = fields.Char()
+    topic = fields.Char()
+    phone = fields.Char()
     classrooms = fields.Many2many(comodel_name='school.classroom',
                                 relation='teachers_classrooms',
                                 column2='classroom_id',
